f885be8346b44784590d9a14448617e44820fa8d
MingyueX <xiemingyue0221@gmail.com>
2023-09-02 23:58:38 -0400

    fix

--- android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
+++ android/src/main/kotlin/io/carius/lars/ar_flutter_plugin/AndroidARView.kt
@@ -9,6 +9,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.HandlerThread
+import android.os.Looper
 import android.util.Log
 import android.view.MotionEvent
 import android.view.PixelCopy
